5cca9f7fa7f655ccb0d8da3e33cb78edace39e82
GleeBoy <1372409074@qq.com>
2020-12-17 19:37:22 +0800

    config

--- cele/models.py
+++ cele/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Widget(models.Model):
+    name = models.CharField('名字', max_length=24)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
