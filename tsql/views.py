0498da5548a829bc05e70c7f9bf3e1187cc43335
GleeBoy <1372409074@qq.com>
2021-11-30 19:45:30 +0800

    test sql

--- tsql/views.py
+++ tsql/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import random
+import random, time
 from tsql.models import Record
 from django.db.models import Max
 
@@ -8,12 +8,16 @@ from django.db.models import Max
 
 def random_insert():
     i = 0
+    t = time.time()
+    t1 = time.time()
     while True:
         Record.objects.create(img_path=str(random.choice(range(9999999999999))), user_id=random.choice(range(100)))
         i += 1
         j = i % 1000
         if j == 0:
-            print(i)
+            t2 = time.time()
+            print(i, round(t2-t1, 2), round(t2-t, 2))
+            t1 = t2
 
 
 
